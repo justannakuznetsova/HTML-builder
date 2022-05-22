bfb4a66a17cd3d9f04d3204ff8a7a0455153bc38
Anna Kuznetsova <90614620+justannakuznetsova@users.noreply.github.com>
2022-05-22 17:38:03 -0500

    feat: update index.js file

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFile = path.join(__dirname, 'text.txt');
+const writtenStream = fs.createWriteStream(pathToFile);
+const { stdin, stdout, exit } = process;
+
+stdout.write('The text you\'re gonna write here will appear in the text.txt file. If you wanna exit the program type Ctrl+C or exit\n');
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    exit();
+  }
+  writtenStream.write(data.toString());
+});
+
+process.on('exit', () => stdout.write('See ya!'));
